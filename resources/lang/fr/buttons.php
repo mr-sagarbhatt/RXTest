a3d28b08b284fe69682bb90b4da0b73baab1b15c
Rushil Prajapati <rushil.prajapati@radixusers2.com>
2021-09-22 21:57:01 +0530

    Completed Task - Multi language and Pagination and Employees modules.

---
+++ resources/lang/fr/buttons.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Buttons Language Lines
+    |--------------------------------------------------------------------------
+    */
+
+    'back' => 'Arrière',
+    'create' => 'Créer',
+    'edit' => 'Éditer',
+    'delete' => 'Effacer',
+    'login' => 'Connexion',
+    'create-company' => 'Créer une entreprise',
+    'create-employee' => 'Créer un employé',
+    'send-password-reset-link' => 'Envoyer le lien de réinitialisation du mot de passe',
+];
